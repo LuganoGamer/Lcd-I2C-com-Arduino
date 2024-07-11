ed0288b98ec4dcc3347606c745ee4677ad068b12
victor <victorgabrielamaralselayaran@gmail.com>
2024-07-11 11:43:31 -0300

    segunda versão

--- amazing_bruticus_lahdi1/amazing_bruticus_lahdi1.ino
+++ amazing_bruticus_lahdi1/amazing_bruticus_lahdi1.ino
@@ -4,8 +4,12 @@ LiquidCrystal_I2C lcd(0x20,16,2);
 void setup(){
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(1,0);
-  lcd.print("Hello, word!");
+  
 }
 
-void loop(){}
+void loop(){
+  for(int i=0;i<=16;i++){
+    lcd.setCursor(i,0);
+    lcd.print("Hello, word!");
+  }
+}
